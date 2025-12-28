fd72e3496ea9d1d55622320237066117f045e164
Marius Danciu <marius.danciu@gmail.com>
2025-12-28 15:12:27 +0200

    support FBM

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+use std::ops::{Add, Mul, Sub};
+
 use glam::Vec2;
 
 use image::{ImageBuffer, Rgb};
 use rand::prelude::*;
 
-fn mix(start: f32, stop: f32, a: f32) -> f32 {
-    start * (1. - a) + stop * a
+fn mix<T>(start: T, stop: T, a: T) -> T
+where
+    T: Mul<T, Output = T> + Sub<T, Output = T> + Add<T, Output = T> + From<f32> + Clone,
+{
+    start * (T::from(1.) - a.clone()) + stop * a
 }
 
-fn anchors(scale: u32) -> Vec<Vec2> {
+fn rnd_anchors(scale: u32) -> Vec<Vec2> {
     let mut anchors: Vec<Vec2> = Vec::with_capacity((scale * scale) as usize);
     let mut rng = rand::rng();
     for _ in 0..scale * scale {
@@ -20,8 +25,8 @@ fn anchors(scale: u32) -> Vec<Vec2> {
     anchors
 }
 
-fn worley_noise(p: Vec2, scale: u32, res: u32, anchors: &Vec<Vec2>) -> f32 {
-    let st = (p / res as f32) * scale as f32;
+fn worley_noise(uv: Vec2, scale: u32, anchors: &Vec<Vec2>) -> f32 {
+    let st = uv * scale as f32;
 
     let current_cell = Vec2::new(st.x.floor(), st.y.floor());
 
@@ -50,16 +55,41 @@ fn worley_noise(p: Vec2, scale: u32, res: u32, anchors: &Vec<Vec2>) -> f32 {
     min_dist
 }
 
+fn fbm(v: Vec2, scale: u32, anchors: &Vec<Vec<Vec2>>) -> f32 {
+    let mut noise = 0.0;
+    let mut amplitude = 1.0;
+    let octaves = 3;
+
+    let mut s = scale;
+
+    for o in 0..octaves {
+        noise += worley_noise(v, s, anchors.get(o).unwrap()) * amplitude;
+        amplitude *= 0.5;
+        s *= 2;
+    }
+    noise
+}
+
 fn main() {
     let res = 500;
-    let scale = 5;
+    let scale = 4;
     let mut imgbuf: ImageBuffer<Rgb<u8>, Vec<_>> = image::ImageBuffer::new(res, res);
 
-    let anchors = &anchors(scale);
+    let anchors = vec![
+        rnd_anchors(scale),
+        rnd_anchors(scale * 2),
+        rnd_anchors(scale * 4),
+        rnd_anchors(scale * 5),
+    ];
 
     for iy in 0..res {
         for ix in 0..res {
-            let min_dist = worley_noise(Vec2::new(ix as f32, iy as f32), scale, res, anchors);
+            let p = Vec2::new(ix as f32, iy as f32);
+            let uv = p / res as f32;
+
+            //let min_dist = worley_noise(uv, scale, res, anchors);
+            let min_dist = fbm(uv, scale, &anchors);
+
             let intensity = mix(255., 0., min_dist) as u8;
 
             let pixel = imgbuf.get_pixel_mut(ix, iy);
