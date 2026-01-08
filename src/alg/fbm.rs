b9d240f144841dde95d91ec339725559cc22b2a2
Marius Danciu <marius.danciu@gmail.com>
2026-01-08 08:19:09 +0200

    unused code

--- src/alg/fbm.rs
+++ src/alg/fbm.rs
@@ -5,7 +5,6 @@ use crate::alg::Noise;
 pub fn fbm(v: Vec2, noises: &mut Vec<impl Noise>, seed: f32) -> Vec3 {
     let mut sum = Vec3::ZERO;
     let mut amplitude = 1.0;
-    let len = noises.len() as f32;
 
     for n in noises {
         sum += n.noise(v, seed) * amplitude;
